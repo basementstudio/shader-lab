4ea460475fc7548f5880a5f44c05e1a8c08f4608
git-chad <tobimocc@gmail.com>
2026-03-27 15:01:49 -0300

    typo

--- src/components/editor/layer-sidebar.tsx
+++ src/components/editor/layer-sidebar.tsx
@@ -75,7 +75,7 @@ const addLayerOptions = [
     label: (
       <span className={menuButtonClassName}>
         <CameraIcon size={14} weight="regular" />
-        Live CameraIcon
+        Camera
       </span>
     ),
     value: "live",
